9cafc338ee37b27ada09bfd0d1b11297e978ee8a
Ash Furrow <ash@ashfurrow.com>
2016-06-28 21:43:51 -0400

    [Testing] Adds linter warning to Swift file.

--- Artsy/Stringify.swift
+++ Artsy/Stringify.swift
@@ -3,7 +3,7 @@ import Foundation
 /// Turns a dictionary into its corresponding JSON representation.
 /// Note: The object must be only Foundational collection classes, strings, and numbers. JSON stuff.
 extension NSObject {
-    func stringify(prettyPrinted: Bool = false) throws -> String {
+    func stringify(prettyPrinted:Bool = false) throws -> String {
         let data = try NSJSONSerialization.dataWithJSONObject(self, options: prettyPrinted ? [.PrettyPrinted] : [])
         // Note we're using the crash! operator. At this point in the function execution, we
         // have successfully encoded ourselves into JSON and decoding into a string should succeed.
